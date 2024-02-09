2379e7e3f905bf182e2ac99fac70c66a00ceda76
BAW_CI_AUTOMATION <noreply+bot@gamestorevn.com>
2024-02-09 13:35:11 +0000

    Publish automated build result from 1.0.0+f45ed4c main

--- dist/ws-api/api/getUserInfo.d.ts
+++ dist/ws-api/api/getUserInfo.d.ts
@@ -8,6 +8,7 @@ declare const func: import("@badaimweeb/js-dtsocket").Procedure<void, {
     nameOrder: "first-last" | "last-first";
     balance: number;
     permission: number;
+    joinedAt: number;
 }, import("@badaimweeb/js-dtsocket").ServerContext<import("../../types.js").GlobalState, import("../../types.js").LocalState, import("../../types.js").EventTable, import("@badaimweeb/js-protov2d").Session<import("ws").WebSocket & {
     req: import("http").IncomingMessage;
 }>>>;
--- dist/ws-api/index.d.ts
+++ dist/ws-api/index.d.ts
@@ -1,6 +1,6 @@
 import getUserInfo from "./api/getUserInfo.js";
-import isEmailVerified from "./api/isEmailVerified.js";
+import getEmails from "./api/getEmails.js";
 import login from "./api/login.js";
 import logout from "./api/logout.js";
 import switchToken from "./api/switchToken.js";
-export { getUserInfo, isEmailVerified, login, logout, switchToken };
+export { getUserInfo, getEmails, login, logout, switchToken };
