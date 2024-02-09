dde3ae02948bc4f71921f1f3d9df76b57fd17dc8
BAW_CI_AUTOMATION <noreply+bot@gamestorevn.com>
2024-02-09 07:46:51 +0000

    Publish automated build result from 1.0.0+3338393 main

--- dist/ws-api/index.d.ts
+++ dist/ws-api/index.d.ts
@@ -1,1 +1,6 @@
-export {};
+import getUserInfo from "./api/getUserInfo.js";
+import isEmailVerified from "./api/isEmailVerified.js";
+import login from "./api/login.js";
+import logout from "./api/logout.js";
+import switchToken from "./api/switchToken.js";
+export { getUserInfo, isEmailVerified, login, logout, switchToken };
