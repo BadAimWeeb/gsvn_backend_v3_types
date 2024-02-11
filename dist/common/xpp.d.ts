9a03845776318d0c7a9d462f8c6d6e791aae7875
BAW_CI_AUTOMATION <noreply+bot@gamestorevn.com>
2024-02-11 07:21:16 +0000

    Publish automated build result from 1.0.0+01fb4e9 main

---
+++ dist/common/xpp.d.ts
@@ -0,0 +1,4 @@
+import { DTSocketClient } from "@badaimweeb/js-dtsocket";
+import ProtoV2dSession from "@badaimweeb/js-protov2d/dist/session";
+declare let xppServer: DTSocketClient<import("@badaimweeb/js-dtsocket").DTSocketServer<import("@badaimweeb/js-dtsocket").ServerContext<import("@badaimweeb/xpp/dist/types.js").GlobalState, import("@badaimweeb/xpp/dist/types.js").LocalState, import("@badaimweeb/xpp/dist/types.js").EventTable, ProtoV2dSession<any>, typeof import("@badaimweeb/xpp/dist/ws-api/index.js")>>, import("@badaimweeb/js-dtsocket").Socket, import("@badaimweeb/js-dtsocket").ServerContext<import("@badaimweeb/xpp/dist/types.js").GlobalState, import("@badaimweeb/xpp/dist/types.js").LocalState, import("@badaimweeb/xpp/dist/types.js").EventTable, ProtoV2dSession<any>, typeof import("@badaimweeb/xpp/dist/ws-api/index.js")>>;
+export default xppServer;
---
+++ dist/common/xppPhoneCardTelcoMap.d.ts
@@ -0,0 +1,2 @@
+export declare const normalizedMap: Record<string, string>;
+export declare const displayName: Record<string, string>;
