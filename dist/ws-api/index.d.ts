a20e06ca651ea6fb53597d992deb0282a0c8eca6
BAW_CI_AUTOMATION <noreply+bot@gamestorevn.com>
2024-02-11 00:01:58 +0000

    Publish automated build result from 1.0.0+8e7a288 main

--- dist/ws-api/index.d.ts
+++ dist/ws-api/index.d.ts
@@ -6,6 +6,7 @@ import getUserInfo from "./api/general/getUserInfo.js";
 import login from "./api/general/login.js";
 import logout from "./api/general/logout.js";
 import switchToken from "./api/general/switchToken.js";
+import admin_updateGamepasses from "./api/admin/admin_updateGamepasses.js";
 import admin_updateRobuxLimit from "./api/admin/admin_updateRobuxLimit.js";
 import admin_updateRobuxRate from "./api/admin/admin_updateRobuxRate.js";
-export { getEmails, getRobuxLimit, getRobuxRate, getUserInfo, getTransactions, login, logout, switchToken, admin_updateRobuxLimit, admin_updateRobuxRate };
+export { getEmails, getRobuxLimit, getRobuxRate, getUserInfo, getTransactions, login, logout, switchToken, admin_updateGamepasses, admin_updateRobuxLimit, admin_updateRobuxRate };
