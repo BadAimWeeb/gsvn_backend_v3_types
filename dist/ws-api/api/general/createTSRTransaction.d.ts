6b055f6e098ee00250c4b3cf497df011f0d29304
BAW_CI_AUTOMATION <noreply+bot@gamestorevn.com>
2024-02-12 07:01:02 +0000

    Publish automated build result from 1.0.0+7c8377f main

--- dist/ws-api/api/general/createTSRTransaction.d.ts
+++ dist/ws-api/api/general/createTSRTransaction.d.ts
@@ -7,6 +7,7 @@ declare const func: import("@badaimweeb/js-dtsocket").Procedure<{
     amount: number;
     message: string;
     timeout: number;
+    localID: string;
 }], import("@badaimweeb/js-dtsocket").ServerContext<import("../../../types.js").GlobalState, import("../../../types.js").LocalState, import("../../../types.js").EventTable, import("@badaimweeb/js-protov2d").Session<import("ws").WebSocket & {
     req: import("http").IncomingMessage;
 }>>>;
--- dist/ws-api/api/general/createVNBankTransaction.d.ts
+++ dist/ws-api/api/general/createVNBankTransaction.d.ts
@@ -8,7 +8,10 @@ declare const func: import("@badaimweeb/js-dtsocket").Procedure<{
     amount: number;
     message: string;
     timeout: number;
+    localID: string;
     qr: string;
+    bank: string;
+    vqrDeeplink: string;
 }], import("@badaimweeb/js-dtsocket").ServerContext<import("../../../types.js").GlobalState, import("../../../types.js").LocalState, import("../../../types.js").EventTable, import("@badaimweeb/js-protov2d").Session<import("ws").WebSocket & {
     req: import("http").IncomingMessage;
 }>>>;
