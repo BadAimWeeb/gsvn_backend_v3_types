f65b12f2b781286cafcfad73ba7e9b476bd2e2a5
BAW_CI_AUTOMATION <noreply+bot@gamestorevn.com>
2024-02-13 11:46:19 +0000

    Publish automated build result from 1.0.0+75b2fcb main

--- dist/ws-api/api/admin/admin_changePurchaseStatus.d.ts
+++ dist/ws-api/api/admin/admin_changePurchaseStatus.d.ts
@@ -1,9 +1,10 @@
 /// <reference types="ws" />
 /// <reference types="node" />
+import { ErrorCode } from "../../../types.js";
 declare const func: import("@badaimweeb/js-dtsocket").Procedure<{
     pcid: number;
     status: "pending" | "processing" | "done" | "cancelled" | "partial-processed";
-}, import("mongodb").WithId<{
+}, (Pick<{
     pcid: number;
     target: string;
     value: number;
@@ -12,7 +13,7 @@ declare const func: import("@badaimweeb/js-dtsocket").Procedure<{
     internalNote?: string | undefined;
     createdAt: number;
     updatedAt: number;
-} & ({
+} & {
     type: "robux";
     data: {
         amountTaxed: number;
@@ -21,7 +22,18 @@ declare const func: import("@badaimweeb/js-dtsocket").Procedure<{
         isSVV: boolean;
     };
     partialProcessedData?: null | undefined;
-} | {
+}, "target" | "value" | "pcid" | "createdAt" | "data" | "status" | "updatedAt" | "type" | "note" | "internalNote" | "partialProcessedData"> & {
+    _id: import("bson").ObjectId;
+}) | (Pick<{
+    pcid: number;
+    target: string;
+    value: number;
+    status: "pending" | "processing" | "done" | "cancelled" | "partial-processed";
+    note?: string | undefined;
+    internalNote?: string | undefined;
+    createdAt: number;
+    updatedAt: number;
+} & {
     type: "roblox-gamepass";
     data: {
         game: string;
@@ -40,7 +52,9 @@ declare const func: import("@badaimweeb/js-dtsocket").Procedure<{
     partialProcessedData?: {
         passesProcessed: string[];
     } | undefined;
-})> | null, import("@badaimweeb/js-dtsocket").ServerContext<import("../../../types.js").GlobalState, import("../../../types.js").LocalState, import("../../../types.js").EventTable, import("@badaimweeb/js-protov2d").Session<import("ws").WebSocket & {
+}, "target" | "value" | "pcid" | "createdAt" | "data" | "status" | "updatedAt" | "type" | "note" | "internalNote" | "partialProcessedData"> & {
+    _id: import("bson").ObjectId;
+}) | ErrorCode[] | null, import("@badaimweeb/js-dtsocket").ServerContext<import("../../../types.js").GlobalState, import("../../../types.js").LocalState, import("../../../types.js").EventTable, import("@badaimweeb/js-protov2d").Session<import("ws").WebSocket & {
     req: import("http").IncomingMessage;
 }>>>;
 export default func;
