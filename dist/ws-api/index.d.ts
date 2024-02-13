810e00911ee85851dace7468dca4d745fd6f4ca2
BAW_CI_AUTOMATION <noreply+bot@gamestorevn.com>
2024-02-13 12:06:37 +0000

    Publish automated build result from 1.0.0+a77c4d5 main

--- dist/ws-api/index.d.ts
+++ dist/ws-api/index.d.ts
@@ -30,9 +30,10 @@ import admin_changeUserPermission from "./api/admin/admin_changeUserPermission.j
 import admin_createTransaction from "./api/admin/admin_createTransaction.js";
 import admin_getPurchase from "./api/admin/admin_getPurchase.js";
 import admin_getPurchases from "./api/admin/admin_getPurchases.js";
+import admin_getRobuxRequired from "./api/admin/admin_getRobuxRequired.js";
 import admin_getUser from "./api/admin/admin_getUser.js";
 import admin_getUserList from "./api/admin/admin_getUserList.js";
 import admin_updateGamepasses from "./api/admin/admin_updateGamepasses.js";
 import admin_updateRobuxLimit from "./api/admin/admin_updateRobuxLimit.js";
 import admin_updateRobuxRate from "./api/admin/admin_updateRobuxRate.js";
-export { buyGamepass, buyRobux, createTSRTransaction, createVNBankTransaction, depositPhoneCard, generateOldLoginClaim, getEmails, getGamepassCreationLink, getGamepasses, getLatestPurchases, getPayments, getPrivacySetting, getPurchases, getRobuxLimit, getRobuxRate, getTopDeposit, getUserInfo, getVNBanks, getVNPhoneCardDisplayName, getVNPhoneCardFee, getTransactions, login, logout, setPrivacySetting, switchToken, admin_changePurchaseNote, admin_changePurchaseNoteInternal, admin_changePurchaseStatus, admin_changeUserPermission, admin_createTransaction, admin_getPurchase, admin_getPurchases, admin_getUser, admin_getUserList, admin_updateGamepasses, admin_updateRobuxLimit, admin_updateRobuxRate };
+export { buyGamepass, buyRobux, createTSRTransaction, createVNBankTransaction, depositPhoneCard, generateOldLoginClaim, getEmails, getGamepassCreationLink, getGamepasses, getLatestPurchases, getPayments, getPrivacySetting, getPurchases, getRobuxLimit, getRobuxRate, getTopDeposit, getUserInfo, getVNBanks, getVNPhoneCardDisplayName, getVNPhoneCardFee, getTransactions, login, logout, setPrivacySetting, switchToken, admin_changePurchaseNote, admin_changePurchaseNoteInternal, admin_changePurchaseStatus, admin_changeUserPermission, admin_createTransaction, admin_getPurchase, admin_getPurchases, admin_getRobuxRequired, admin_getUser, admin_getUserList, admin_updateGamepasses, admin_updateRobuxLimit, admin_updateRobuxRate };
