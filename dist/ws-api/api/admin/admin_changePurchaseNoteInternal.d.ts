6bdbb67443369a16cd02e91e195978ca23054bbe
BAW_CI_AUTOMATION <noreply+bot@gamestorevn.com>
2024-02-13 08:57:02 +0000

    Publish automated build result from 1.0.0+e3d5295 main

---
+++ dist/ws-api/api/admin/admin_changePurchaseNoteInternal.d.ts
@@ -0,0 +1,46 @@
+/// <reference types="ws" />
+/// <reference types="node" />
+declare const func: import("@badaimweeb/js-dtsocket").Procedure<{
+    pcid: number;
+    note: string;
+}, import("mongodb").WithId<{
+    pcid: number;
+    target: string;
+    value: number;
+    status: "pending" | "processing" | "done" | "cancelled" | "partial-processed";
+    note?: string | undefined;
+    internalNote?: string | undefined;
+    createdAt: number;
+    updatedAt: number;
+} & ({
+    type: "robux";
+    data: {
+        amountTaxed: number;
+        amountUntaxed: number;
+        gamepassID: string;
+        isSVV: boolean;
+    };
+    partialProcessedData?: null | undefined;
+} | {
+    type: "roblox-gamepass";
+    data: {
+        game: string;
+        passes: string[];
+        cachedGameName: string;
+        cachedGamePassesName: {
+            [pass: string]: string;
+        };
+        passesRefundValue: {
+            [pass: string]: number;
+        };
+        username: string;
+        password: string;
+        amountRobux: number;
+    };
+    partialProcessedData?: {
+        passesProcessed: string[];
+    } | undefined;
+})> | null, import("@badaimweeb/js-dtsocket").ServerContext<import("../../../types.js").GlobalState, import("../../../types.js").LocalState, import("../../../types.js").EventTable, import("@badaimweeb/js-protov2d").Session<import("ws").WebSocket & {
+    req: import("http").IncomingMessage;
+}>>>;
+export default func;
